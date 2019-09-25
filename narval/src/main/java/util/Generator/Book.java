50cf749dc0db8a9d96de6657e3d1c173e37fde9e
krisztiankover <krisztian.kover.31@gmail.com>
2019-09-25 19:52:08 +0200

    Book.java osztály hozzáadva a projekthez

---
+++ narval/src/main/java/util/Generator/Book.java
@@ -0,0 +1,43 @@
+package util.Generator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * Class representing a book in the library.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Book
+{
+    /**
+     * The Cut-code of the book.
+     */
+    @Id
+    private String id;
+
+    /**
+     * The title of the book.
+     */
+    @Column(nullable = false)
+    private String title;
+
+    /**
+     * The author of the book.
+     */
+    @Column(nullable = false)
+    private String author;
+
+    /**
+     * A marker which is meant to show whenever the book is rented.
+     */
+    @Column(nullable = false)
+    private Boolean available;
+}
