04e41778239172ed733a665e450c0807b607be96
Kyouka <kyoukaa98@gmail.com>
2019-09-27 12:03:01 +0200

    UserDAO bővítése egy új metódussal: user lekérése id alapján

--- narval/src/main/java/dao/UserDAO.java
+++ narval/src/main/java/dao/UserDAO.java
@@ -3,7 +3,6 @@ package dao;
 import util.jpa.GenericJpaDao;
 import model.User;
 
-import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -15,5 +14,11 @@ public class UserDAO extends GenericJpaDao {
         TypedQuery<String> query = entityManager.createQuery("select u.id || ':' || u.name from User u", String.class);
         List<String> result = query.getResultList();
         return !result.isEmpty() ? result : null;
+    }
+
+    public User getUserById(String userId) {
+        TypedQuery<User> query = entityManager.createQuery("select u from User u where u.id = " + userId, User.class);
+        User result = query.getResultList().get(0);
+        return result;
     }
 }
