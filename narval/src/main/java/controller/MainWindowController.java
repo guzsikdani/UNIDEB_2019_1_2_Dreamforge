020abade33dd74969a50f01d454a3f328550227e
danidd2 <dani_sarvari@yahoo.com>
2019-09-27 14:06:46 +0200

    A fő ablakban Kölcsönzés gomb hozzáadva

---
+++ narval/src/main/java/controller/MainWindowController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import dao.BookDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import util.SearchBy;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable
+{
+    @FXML
+    private ComboBox searchByDropdown;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Button rentButton;
+
+    private BookDAO bookDAO;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        fillDropDown();
+        rentButton.setText("Kölcsönzés");
+        openRent();
+    }
+
+    private void fillDropDown()
+    {
+        List<String> options  = new ArrayList<String>(Arrays.asList("Író","Cím","Kód"));
+        ObservableList<String> dropdownOptions = FXCollections.observableArrayList(options);
+        searchByDropdown.setItems(dropdownOptions);
+    }
+
+    private void openRent()
+    {
+        rentButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Parent parent;
+                try {
+                    parent = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/RentWindow.fxml"));
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(parent));
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+}
