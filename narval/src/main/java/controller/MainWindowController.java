8dbb522b377144a9c6b5d45f743c53adfe314874
krisztiankover <krisztian.kover.31@gmail.com>
2019-09-27 22:58:35 +0200

    Keresési felület keresésének implementálása, táblázat feltöltése találatokkal

--- narval/src/main/java/controller/MainWindowController.java
+++ narval/src/main/java/controller/MainWindowController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import dao.BookDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,10 +13,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Modality;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import model.Book;
 import util.SearchBy;
+import util.guice.PersistenceModule;
 
 import java.io.IOException;
 import java.net.URL;
@@ -35,6 +39,18 @@ public class MainWindowController implements Initializable
     private Button searchButton;
 
     @FXML
+    private TableView<Book> resultTable;
+
+    @FXML
+    private TableColumn<Book, String> bookId;
+
+    @FXML
+    private TableColumn<Book, String> bookTitle;
+
+    @FXML
+    private TableColumn<Book, String> bookAuthor;
+
+    @FXML
     private Button rentButton;
 
     @FXML
@@ -42,11 +58,18 @@ public class MainWindowController implements Initializable
 
     private BookDAO bookDAO;
 
-    private Boolean showrent = false;
-    private Boolean showinformation = false;
+    private Boolean showRent = false;
+    private Boolean showInformation = false;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Injector injector = Guice.createInjector(new PersistenceModule("jpa-persistence-unit-1"));
+
+        bookDAO = injector.getInstance(BookDAO.class);
+
+        bookId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        bookTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        bookAuthor.setCellValueFactory(new PropertyValueFactory<>("author"));
 
         fillDropDown();
         rentButton.setText("Kölcsönzés");
@@ -69,16 +92,16 @@ public class MainWindowController implements Initializable
             public void handle(ActionEvent actionEvent) {
                 Parent parent;
                 try {
-                    if (showrent == false) {
+                    if (showRent == false) {
                         parent = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/RentWindow.fxml"));
                         Stage stage = new Stage();
                         stage.setScene(new Scene(parent));
                         stage.show();
-                        showrent = true;
+                        showRent = true;
                         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
                             @Override
                             public void handle(WindowEvent windowEvent) {
-                                showrent = false;
+                                showRent = false;
                             }
                         });
                     }
@@ -96,16 +119,16 @@ public class MainWindowController implements Initializable
             public void handle(ActionEvent actionEvent) {
                 Parent parent;
                 try {
-                    if (showinformation == false) {
+                    if (showInformation == false) {
                         parent = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/InformationWindow.fxml"));
                         Stage stage = new Stage();
                         stage.setScene(new Scene(parent));
                         stage.show();
-                        showinformation = true;
+                        showInformation = true;
                         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
                             @Override
                             public void handle(WindowEvent windowEvent) {
-                                showinformation = false;
+                                showInformation = false;
                             }
                         });
                     }
@@ -116,5 +139,20 @@ public class MainWindowController implements Initializable
         });
     }
 
-
+    @FXML
+    public void search(ActionEvent actionEvent) {
+        if (searchByDropdown.getSelectionModel().getSelectedItem() != null) {
+            List<Book> resultList = new ArrayList<>();
+            String searchTerm = searchByDropdown.getSelectionModel().getSelectedItem().toString();
+            if (searchTerm.equals("Író")) {
+                resultList = bookDAO.bookRead(SearchBy.AUTHOR, searchField.getText());
+            } else if (searchTerm.equals("Cím")) {
+                resultList = bookDAO.bookRead(SearchBy.TITLE, searchField.getText());
+            } else if (searchTerm.equals("Kód")) {
+                resultList = bookDAO.bookRead(SearchBy.ID, searchField.getText());
+            }
+            ObservableList<Book> result = FXCollections.observableArrayList(resultList);
+            resultTable.setItems(result);
+        }
+    }
 }
